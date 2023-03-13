eb443f2ba44755d2ad9c0a642a1df6e42e3a7265
MatheusFinatto <matheusfinatto2@gmail.com>
2023-03-13 17:21:05 -0300

    feat:added side nav for mobile support

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/top-games')
+def top_games():
+    return render_template('top-games.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
