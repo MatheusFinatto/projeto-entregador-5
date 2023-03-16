4c0230517a6982ff04ccd9a1e81e4f039f2625b2
MatheusFinatto <matheusfinatto2@gmail.com>
2023-03-16 18:00:43 -0300

    fix:added the right query to best games

--- routes_functions/top_games.py
+++ routes_functions/top_games.py
@@ -9,8 +9,8 @@ HEADERS = {
 
 
 def top_games():
-    url = 'https://api.igdb.com/v4/covers'
-    data = f'fields url, game.name; limit 50;'
+    url = 'https://api.igdb.com/v4/games'
+    data = f'fields name, cover.url, rating, rating_count; where rating != null & rating_count != null & rating_count > 1000; sort rating desc; limit 50;'
     headers = HEADERS
     response = requests.post(url, headers=headers, data=data)
     newJson = []
@@ -18,9 +18,9 @@ def top_games():
         # response.json() é um array de objetos (url de imagens)
         newJson = response.json().copy()
         for i in range(len(response.json())):
-            newJson[i]["url"] = response.json(
-            )[i]['url'].replace("t_thumb", "t_1080p")
-
+            newJson[i]['cover']["url"] = response.json(
+            )[i]['cover']["url"].replace("t_thumb", "t_1080p")
+        print(newJson)
         return render_template('top-games.html', newJson=newJson)
     else:
         return jsonify({'error': 'Failed to retrieve game cover.'}), response.status_code
