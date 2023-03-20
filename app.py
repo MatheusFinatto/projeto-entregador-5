0105527327969bdf949a789c693fffe1fa5d7dc4
Lowez <luizgtadisarz@gmail.com>
2023-03-20 17:28:39 -0300

    Começando o dev da página de login

--- app.py
+++ app.py
@@ -44,5 +44,13 @@ def index():
     return render_template('index.html', posts=posts)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return null
+    else:
+        return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
