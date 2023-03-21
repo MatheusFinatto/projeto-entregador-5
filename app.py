4f6c3751270fa4c2e120a43487a115ccb0d52570
Lowez <luizgtadisarz@gmail.com>
2023-03-21 02:48:40 -0300

    Adicionando tela de registro de usuário e utilizando jquery para verificar senha

--- app.py
+++ app.py
@@ -51,6 +51,12 @@ def login():
     else:
         return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        return null
+    else:
+        return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
