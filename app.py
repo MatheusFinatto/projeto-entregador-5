1ad0a994a8c5df4905b51b24bfaab9c8a1816b9c
MatheusFinatto <matheusfinatto2@gmail.com>
2023-04-03 18:01:29 -0300

    wip: adding 404 page

--- app.py
+++ app.py
@@ -285,5 +285,11 @@ def index():
     return render_template('index.html')
 
 
+# 404 #
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
